335345e9d001fab54d3dd43043111bfcfc31c0db
lawshe <jessica@jlawshe.com>
2017-08-16 16:43:28 -0500

    Add input tests

--- __tests__/redux.js
+++ __tests__/redux.js
@@ -2,7 +2,8 @@ import * as actions from '../app/actions/actionCreators';
 import * as types from '../app/actions/actionTypes';
 import reducer from '../app/reducers/index'
 
-describe('actions', () => {
+// Begin accessibility tests
+describe('accessibility actions', () => {
   it('should create an action to toggle parallax', () => {
     const expectedAction = {
       type: types.TOGGLE_PARALLAX
@@ -11,7 +12,7 @@ describe('actions', () => {
   });
 });
 
-describe('accessibility reducer', () => {
+describe('accessibility reducers', () => {
   it('should return the initial state', () => {
     expect(reducer.accessibility(undefined, {})).toEqual({
       parallax: true
@@ -26,3 +27,35 @@ describe('accessibility reducer', () => {
     })
   });
 });
+// End accessibility tests
+
+// Begin input tests
+describe('input actions', () => {
+  it('should update text', () => {
+    const input_text = 'A robot may not injure a human being';
+    const expectedAction = {
+      type: types.UPDATE_TEXT,
+      input_text
+    };
+    expect(actions.updateText(input_text)).toEqual(expectedAction)
+  });
+});
+
+describe('input reducers', () => {
+  it('should return the initial state', () => {
+    expect(reducer.input(undefined, {})).toEqual({
+      input_text: ''
+    })
+  });
+
+  it('should update the text', () => {
+    const input_text = 'A robot must obey the orders given it by human beings';
+    expect(reducer.input(undefined, {
+      type: types.UPDATE_TEXT,
+      input_text: input_text
+    })).toEqual({
+      input_text
+    })
+  });
+});
+// End input tests
