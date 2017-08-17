6c67acd32f14b30454954120bcc5fbfabc72eced
lawshe <jessica@jlawshe.com>
2017-08-17 16:25:35 -0500

    Add getLastWord()
    Using in findUser() and also in InputText component for message when no
    users found

--- utils/fxns.js
+++ utils/fxns.js
@@ -1,12 +1,15 @@
 import usersData from '../data/sorted-usernames';
 
+const getLastWord = (text) => {
+  return text.split(' ').pop().toLowerCase();
+}
+
 const findUser = (inputText) => {
   // This is run after each text input change
   // Only search for users based on last word in input, when last word is more than 1 character
   // Return only 3 user suggestions max
-
   let userResults = [];
-  let lastWord = inputText.split(' ').pop().toLowerCase();
+  let lastWord = getLastWord(inputText);
 
   if (lastWord.trim() !== '' && lastWord.length > 1) {
     lastWord = lastWord.replace('@', '');
@@ -36,5 +39,6 @@ const includeUser = (inputText, username) => {
 
 export default {
   findUser,
+  getLastWord,
   includeUser
 }
