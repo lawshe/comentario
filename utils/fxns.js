97a9e3379614c11b72b8c1ed1da1147c772b5e79
lawshe <jessica@jlawshe.com>
2017-08-17 11:14:21 -0500

    Add `findUser()`
    A function to match the last word in an HTML input with user data.
    Using a regex to match start of user.name or user.username with last
    word. Only returning 3 user suggestions max.

--- utils/fxns.js
+++ utils/fxns.js
@@ -1,3 +1,32 @@
-export default {
+import usersData from '../data/data';
 
+const findUser = (inputText) => {
+  // This is run after each input change
+  // Only search for users based on last word in input, when last word is more than 1 character
+  // Return only 3 user suggestions max
+
+  let userResults = [];
+  const lastWord = inputText.split(' ').pop().toLowerCase().replace('@', '');
+  const regexPtn = '^' + lastWord + '[a-zA-Z0-9]+';
+
+  if (lastWord.trim() !== '' && lastWord.length > 1) {
+    usersData.forEach((user, index) => {
+      if (userResults.length < 2) {
+        for (let [key, value] of Object.entries(user)) {
+          if (key === 'username' || key === 'name') {
+            if (value.match(regexPtn)) {
+              userResults.push(user);
+            }
+          }
+        }
+      }
+    });
+  }
+
+  return userResults;
+}
+
+export default {
+  findUser,
+  includeUser
 }
