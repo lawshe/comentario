9a1ff34aa9a5f64113eec50909116f294c51d435
lawshe <jessica@jlawshe.com>
2017-08-17 11:20:04 -0500

    InputText component - Show user suggestions
    Also changed <textarea></textarea> to <input></input>

--- app/components/partials/InputText.js
+++ app/components/partials/InputText.js
@@ -13,7 +13,8 @@ import glob from 'style';
 
 const mapStateToProps = (state) => {
   return {
-    inputText: state.input.input_text
+    inputText: state.input.input_text,
+    matchedUsers: state.input.matched_users
   }
 }
 
@@ -29,18 +30,40 @@ class TextInput extends React.Component {
 
   componentDidMount() {
     this.props.onTextChange('');
+    $('.tooltipped').tooltip({delay: 50});
   }
 
   handleTextChange() {
-    const textValue = document.getElementById('textarea1').value;
+    const textValue = document.getElementById('input-el').value;
     this.props.onTextChange(textValue);
   }
 
   render() {
+    const usersJsx = this.props.matchedUsers && this.props.matchedUsers.length > 0 ? (
+      <ul>
+        {this.props.matchedUsers.map((user, index) => {
+          return (
+            <li key={index}>
+              <div className="chip">
+                <img src={user.avatar_url} alt={user.username} />
+                {user.username}
+              </div>
+            </li>
+          );
+        })}
+      </ul>)
+    : <span></span>;
+
     return (
       <div className="card"><div className="card-content">
-        <textarea className="materialize-textarea" id="textarea1" onChange={() => this.handleTextChange()}>{this.props.inputText}</textarea>
-        <label htmlFor="textarea1">Text Input</label>
+        <input id="input-el"
+          onChange={() => this.handleTextChange()}
+          placeholder="Write a comment..."
+          style={{ marginBottom: 0 }}
+          value={this.props.inputText}
+        ></input>
+        <label htmlFor="input-el" style={{ display: 'none', float: 'left', clear: 'both', width: '100%' }}>Comment</label>
+        {usersJsx}
       </div></div>
     );
   }
