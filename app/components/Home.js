1bdd054e236c9808244d68d1b61ffec3f82c3051
lawshe <jessica@jlawshe.com>
2017-08-17 13:52:24 -0500

    Display saved comments

--- app/components/Home.js
+++ app/components/Home.js
@@ -15,6 +15,7 @@ import glob from 'style';
 
 const mapStateToProps = (state) => {
   return {
+    comments: state.input.comments,
     parallax: state.accessibility.parallax
   }
 }
@@ -27,16 +28,35 @@ class Home extends React.Component {
   }
 
   render() {
+    const commentsJSX = this.props.comments && this.props.comments.length > 0 ? (
+      <div className={`section container`}>
+        <div className="row"><div className="col s12">
+          <h3 className="header grey-text text-lighten-5" style={{ fontWeight: 'normal', marginTop: 0 }}>Comments</h3>
+          <ul className="collection">
+            {this.props.comments.map((comment, index) => {
+              return (
+                <li className="collection-item" key={`comment-${index}`}>
+                  {comment}
+                </li>
+              );
+            })}
+          </ul>
+        </div></div>
+      </div>)
+    : <span></span>;
+
     return (
       <div>
         <Intro parallax={this.props.parallax} />
-        <div className={`section ${glob.no_pad} green darken-4 container`}>
-          <div className={`row ${glob.no_margin} ${glob.pad_md}`}>
+        <div className={`section container ${glob.pad_md_top_btm}`}>
+          <h3 className="header grey-text text-lighten-5" style={{ fontWeight: 'normal', marginTop: 0 }}>Add a Comment</h3>
+          <div className={`row ${glob.no_margin}`}>
             <div className={`col s12 `}>
               <InputText />
             </div>
           </div>
         </div>
+        {commentsJSX}
       </div>
     )
   }
