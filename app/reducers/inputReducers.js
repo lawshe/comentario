474c11e2a28e1432e078a0cd314752b47decd3e3
lawshe <jessica@jlawshe.com>
2017-08-17 11:16:50 -0500

    UPDATE_TEXT reducer - Search for users
    In reducer to update text input state, also search for user using
    `findUser()`

--- app/reducers/inputReducers.js
+++ app/reducers/inputReducers.js
@@ -2,13 +2,15 @@ import { UPDATE_TEXT } from '../actions/actionTypes';
 import fxns from '../../utils/fxns';
 
 const initialState = {
-  input_text: '' // User input
+  input_text: '',
+  matched_user: []
 };
 
 export default function inputReducers(state = initialState, action) {
   switch (action.type) {
     case UPDATE_TEXT: {
-      return Object.assign({}, state, { input_text: action.input_text });
+      const matched_users = fxns.findUser(action.input_text);
+      return Object.assign({}, state, { input_text: action.input_text, matched_users });
     }
     default:
       return state;
