00c2889fd7c21eef912ef196d5daf214114c4934
lawshe <jessica@jlawshe.com>
2017-08-17 12:02:48 -0500

    Support @mentions

--- utils/fxns.js
+++ utils/fxns.js
@@ -6,10 +6,12 @@ const findUser = (inputText) => {
   // Return only 3 user suggestions max
 
   let userResults = [];
-  const lastWord = inputText.split(' ').pop().toLowerCase().replace('@', '');
-  const regexPtn = '^' + lastWord + '[a-zA-Z0-9]+';
+  let lastWord = inputText.split(' ').pop().toLowerCase();
 
   if (lastWord.trim() !== '' && lastWord.length > 1) {
+    lastWord = lastWord.replace('@', '');
+    const regexPtn = '^' + lastWord + '[a-zA-Z0-9]+';
+
     usersData.forEach((user, index) => {
       if (userResults.length < 2) {
         for (let [key, value] of Object.entries(user)) {
