5ad1e0747da777b885cd71efd7c054513fdbdba3
lawshe <jessica@jlawshe.com>
2017-08-17 19:07:03 -0500

    Fix user matching bug
    Bug existed when trying to match lastWord to user.name.
    
    This was caused by the space after the first name in the value and the
    capitalization of first and last name.
    
    Updating the regex pattern to include space and lowercase values fixed
    the bug.

--- utils/fxns.js
+++ utils/fxns.js
@@ -9,16 +9,16 @@ const findUser = (inputText) => {
   // Only search for users based on last word in input, when last word is more than 1 character
   // Return only 3 user suggestions max
   let userResults = [];
-  let lastWord = getLastWord(inputText);
+  let lastWord = getLastWord(inputText).toLowerCase();
 
   if (lastWord.trim() !== '' && lastWord.length > 1) {
     lastWord = lastWord.replace('@', '');
-    const regexPtn = '^' + lastWord + '[a-zA-Z0-9]+';
-
+    const regexPtn = '^' + lastWord + '[a-zA-Z0-9\s]+';
     usersData.forEach((user, index) => {
       if (userResults.length < 3) {
         for (let [key, value] of Object.entries(user)) {
           if (key === 'username' || key === 'name') {
+            value = value.toLowerCase();
             if (value.match(regexPtn)) {
               userResults.push(user);
             }
